10f90cb7e0f7709b9f09ff05528293945c2de0b9
ipoemi <ipoemi@sjinc.co.kr>
2016-05-19 10:29:16 +0900

    Signed-off-by: ipoemi <ipoemi@sjinc.co.kr>

--- src/script/sj/jerp/JewelryProductImageModifier.scala
+++ src/script/sj/jerp/JewelryProductImageModifier.scala
@@ -1,4 +1,4 @@
-package script
+package script.sj.jerp
 
 import java.awt.Color
 import java.awt.image.BufferedImage
--- src/script/sj/logis/SequenceQueryBuilder.scala
+++ src/script/sj/logis/SequenceQueryBuilder.scala
@@ -1,4 +1,4 @@
-package script.logis
+package script.sj.logis
 
 import java.awt.Color
 import java.awt.image.BufferedImage
@@ -16,6 +16,8 @@ object SequenceQueryBuilder extends App {
 	println("--------------- " + this.getClass.getName + " 시작" + " ---------------")
 	println()
 	
+	throw new Exception("""""")
+
 	def queryBuilder(query: String, replacement:String, startYear: String, endYear: String): String = {
 		val resultSQL = new StringBuffer()
 		resultSQL.append(query.replaceAll(replacement, startYear.toString))
@@ -27,35 +29,49 @@ object SequenceQueryBuilder extends App {
 	
 	
 	val strSQL = new StringBuffer();
-	strSQL.append("\nSELECT 회사코드, 센터코드, 창고구분, 스타일, 색상, 규격 ") ;
-	strSQL.append("\n     , DECODE(전표구분, 'A', DECODE(입고구분, '1', NVL(입고수량, 0), 0), 0) 생산입고 ") ;
-	strSQL.append("\n     , DECODE(전표구분, 'A', DECODE(입고구분, '2', NVL(입고수량, 0), 0), 0) 이관입고 ") ;
-	strSQL.append("\n     , DECODE(전표구분, 'A', DECODE(입고구분, '3', NVL(입고수량, 0), 0), 0) 완성입고 ") ;
-	strSQL.append("\n     , DECODE(전표구분, 'A', DECODE(입고구분, '4', NVL(입고수량, 0), 0), 0) 이동입고 ") ;
-	strSQL.append("\n     , DECODE(전표구분, 'A', 0, NVL(입고수량, 0)) 입고반품 ") ;
-	strSQL.append("\n     , 0 완성출고, 0 이동출고, 0 출고수량, 0 초도출고, 0 반응출고 ") ;
-	strSQL.append("\n     , 0 교환출고, 0 수주출고, 0 기타출고, 0 수불출고, 0반품수량 ") ;
+	strSQL.append("\nSELECT A.회사코드, A.센터코드, A.창고구분, A.스타일, A.색상, A.규격 ") ;
+	strSQL.append("\n     , 0 생산입고, 0 이관입고, 0 완성입고 ") ;
+	strSQL.append("\n     , DECODE(이동구분,'A',NVL(이동수량,0),0) 이동입고 ") ;
+	strSQL.append("\n     , 0 입고반품, 0 완성출고 ") ;
+	strSQL.append("\n     , DECODE(이동구분,'B',NVL(이동수량,0),0) 이동출고 ") ;
+	strSQL.append("\n     , 0 출고수량, 0 초도출고, 0 반응출고 ") ;
+	strSQL.append("\n     , 0 교환출고, 0 수주출고, 0 기타출고, 0 수불출고, 0 반품수량 ") ;
 	strSQL.append("\n     , 0 시즌반품, 0 불량반품, 0 수주반품, 0 교환반품, 0 수정반품 ") ;
 	strSQL.append("\n     , 0 수선반품, 0 기타반품, 0 수불반품, 0 재고조정, 0 작업지시 ") ;
 	strSQL.append("\n     , 0 재고수량, 0 LOSS수량 ") ;
-	strSQL.append("\n     , 브랜드존, 브랜드, 라인, 아이템, 년도, 시즌 ") ;
-	strSQL.append("\n  FROM LOGIS_BACKUP.창고입고_제품_{숫자}_세정 ") ;
+	strSQL.append("\n     , NVL(B.브랜드존, A.브랜드존) 브랜드존 ") ;
+	strSQL.append("\n     , NVL(B.브랜드, A.브랜드) 브랜드 ") ;
+	strSQL.append("\n     , B.라인 ") ;
+	strSQL.append("\n     , NVL(B.아이템, A.아이템) 아이템 ") ;
+	strSQL.append("\n     , NVL(B.년도, A.년도) 년도 ") ;
+	strSQL.append("\n     , NVL(B.시즌, A.시즌) 시즌 ") ;
+	strSQL.append("\n  FROM LOGIS_BACKUP.아소트솔리드변경_{숫자}_세정 A ") ;
+	strSQL.append("\n     , 스타일 B ") ;
+	strSQL.append("\n WHERE A.회사코드 = B.회사코드 (+) ") ;
+	strSQL.append("\n   AND A.스타일 = B.스타일 (+) ") ;
 	
-	val sql = new StringBuffer(queryBuilder(strSQL.toString, "\\{숫자\\}", "1995", "2012"))
+	val sql = new StringBuffer(queryBuilder(strSQL.toString, "\\{숫자\\}", "2003", "2012"))
 	sql.append("\n UNION ALL ")
-	sql.append("\nSELECT 회사코드, 센터코드, 창고구분, 스타일, 색상, 규격 ") ;
-	sql.append("\n     , DECODE(전표구분, 'A', DECODE(입고구분, '1', NVL(입고수량, 0), 0), 0) 생산입고 ") ;
-	sql.append("\n     , DECODE(전표구분, 'A', DECODE(입고구분, '2', NVL(입고수량, 0), 0), 0) 이관입고 ") ;
-	sql.append("\n     , DECODE(전표구분, 'A', DECODE(입고구분, '3', NVL(입고수량, 0), 0), 0) 완성입고 ") ;
-	sql.append("\n     , DECODE(전표구분, 'A', DECODE(입고구분, '4', NVL(입고수량, 0), 0), 0) 이동입고 ") ;
-	sql.append("\n     , DECODE(전표구분, 'A', 0, NVL(입고수량, 0)) 입고반품 ") ;
-	sql.append("\n     , 0 완성출고, 0 이동출고, 0 출고수량, 0 초도출고, 0 반응출고 ") ;
-	sql.append("\n     , 0 교환출고, 0 수주출고, 0 기타출고, 0 수불출고, 0반품수량 ") ;
+	sql.append("\nSELECT A.회사코드, A.센터코드, A.창고구분, A.스타일, A.색상, A.규격 ") ;
+	sql.append("\n     , 0 생산입고, 0 이관입고, 0 완성입고 ") ;
+	sql.append("\n     , DECODE(이동구분,'A',NVL(이동수량,0),0) 이동입고 ") ;
+	sql.append("\n     , 0 입고반품, 0 완성출고 ") ;
+	sql.append("\n     , DECODE(이동구분,'B',NVL(이동수량,0),0) 이동출고 ") ;
+	sql.append("\n     , 0 출고수량, 0 초도출고, 0 반응출고 ") ;
+	sql.append("\n     , 0 교환출고, 0 수주출고, 0 기타출고, 0 수불출고, 0 반품수량 ") ;
 	sql.append("\n     , 0 시즌반품, 0 불량반품, 0 수주반품, 0 교환반품, 0 수정반품 ") ;
 	sql.append("\n     , 0 수선반품, 0 기타반품, 0 수불반품, 0 재고조정, 0 작업지시 ") ;
 	sql.append("\n     , 0 재고수량, 0 LOSS수량 ") ;
-	sql.append("\n     , 브랜드존, 브랜드, 라인, 아이템, 년도, 시즌 ") ;
-	sql.append("\n  FROM 창고입고_제품 ") ;
+	sql.append("\n     , NVL(B.브랜드존, A.브랜드존) 브랜드존 ") ;
+	sql.append("\n     , NVL(B.브랜드, A.브랜드) 브랜드 ") ;
+	sql.append("\n     , B.라인 ") ;
+	sql.append("\n     , NVL(B.아이템, A.아이템) 아이템 ") ;
+	sql.append("\n     , NVL(B.년도, A.년도) 년도 ") ;
+	sql.append("\n     , NVL(B.시즌, A.시즌) 시즌 ") ;
+	sql.append("\n  FROM 아소트솔리드변경 A ") ;
+	sql.append("\n     , 스타일 B ") ;
+	sql.append("\n WHERE A.회사코드 = B.회사코드 (+) ") ;
+	sql.append("\n   AND A.스타일 = B.스타일 (+) ") ;
 	
 	println(sql)
 	
